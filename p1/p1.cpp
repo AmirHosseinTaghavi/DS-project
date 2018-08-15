620c68fa44520686e04ca1ecb5a7998f9c5cd3d4
amirhtk1 <a.taghavi74@gmail.com>
2018-08-15 19:53:50 +0430

    update p1

--- p1/p1.cpp
+++ p1/p1.cpp
@@ -2,46 +2,17 @@
 #include <fstream>
 
 #include "myLib/Computing.h"
+#include "myLib/Data.h"
 
-void sumMatrices(Computing &c, int row, int col); 
+const std::string MY_IP = "tcp://localhost:5550"; 
 
 int main()
 {
-	int inputArr[24];
-	std::ifstream inputFile("input.txt");
-	if(inputFile.is_open()){	
-		for(int i=0; i<24; ++i){
-			inputFile >> inputArr[i];
-		}
-	}
-	inputFile.close();
+	Data d(MY_IP);
+	d.getMyShare();	
+	
+	Computing c(MY_IP);
+	c.ready4DataReq(d.getMyPart());
 
-	Computing comp(3, 0);
-	comp.distribute(inputArr, 24);
-	//sumMatrices(comp, 3, 4);
-	std::unique_ptr<int[]> p;	
-	p = comp.access(5,6);
-	for(int i=0; i<6; ++i){
-		std::cout << p[i] << std::endl;
-	}
     return 0;
-}
-
-void sumMatrices(Computing &c, int row, int col){
-	int mat[row][col];
-	int matSize = row*col;
-	int index = 0;
-	for(int i=0; i<row; ++i){
-		for(int j=0; j<col; ++j){
-			mat[i][j] = c.access(index) + c.access(index+matSize);
-			++index;
-		}
-	}
-	std::cout<< "output matrice: " <<std::endl;
-	for(int i=0; i<row; ++i){
-		for(int j=0; j<col; ++j){
-			std::cout<< mat[i][j] << ",";
-		}
-		std::cout<< "\n" ;
-	}
 }
